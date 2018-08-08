35b3d46e72d0be224a12838df0f87713851db57d
Julius Bade and Mark Mangahas <julius@>
2018-08-08 09:21:12 -0700

    dom practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,32 @@
 $(document).ready(function() {
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>");
 
-  $("button").click(function(){
-    $(".tabcontent").hide();
-  })
-  
-  $("button.click1").click(function() {
-      $("#java").fadeToggle();
-
+    $("ul#user").children("li").first().click(function() {
+    $(this).remove();
    });
-
-  $("button.click2").click(function() {
-      $("#ops").fadeToggle();
-
+  $("ul#webpage").children("li").first().click(function() {
+    $(this).remove();
    });
-
-
-  $("button.click3").click(function() {
-    $("#Var").fadeToggle();
-
  });
 
- $("button.click4").click(function() {
-   $("#VarNam").fadeToggle();
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
 
-});  $("button.click5").click(function() {
-    $("#Fu").fadeToggle();
+    $("ul#user").children("li").first().click(function() {
+    $(this).remove();
+   });
+    $("ul#webpage").children("li").first().click(function() {
+    $(this).remove();
+   });
+ });
 
- });  $("button.click6").click(function() {
-     $("#Meth").fadeToggle();
 
-  });  $("button.click7").click(function() {
-      $("#Arg").fadeToggle();
+  $("button#stop").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
 
-   });  $("button.click8").click(function() {
-       $("#Par").fadeToggle();
-
-    });  $("button.click9").click(function() {
-        $("#Ret").fadeToggle();
-
-     });  $("button.click10").click(function() {
-         $("#Chain").fadeToggle();
-
-      });  $("button.click11").click(function() {
-          $("#Str").fadeToggle();
-
-       });  $("button.click12").click(function() {
-           $("#boo").fadeToggle();
-
-        });  $("button.click13").click(function() {
-            $("#Und").fadeToggle();
-
-         });  $("button.click14").click(function() {
-             $("#NaN").fadeToggle();
-
-          });  $("button.click15").click(function() {
-              $("#esc").fadeToggle();
-
-           });  $("button.click16").click(function() {
-               $("#Als").fadeToggle();
-
-             });  $("button.click17").click(function() {
-                 $("#Com").fadeToggle();
-
-               });  $("button.click18").click(function() {
-                   $("#JQ").fadeToggle();
-
-                 });  $("button.click19").click(function() {
-                     $("#Att").fadeToggle();
-
-            });
-});
+   });
+ });
